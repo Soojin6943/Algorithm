f76a5c7472122d6ee4d01fdf0e6fdd2392bd14bd
Soojin6943 <159032295+Soojin6943@users.noreply.github.com>
2025-04-30 09:50:15 +0900

    [level 0] Title: 문자열로 변환, Time: 0.03 ms, Memory: 70.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
+++ Java/프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -1,6 +1,10 @@
 class Solution {
     public String solution(int n) {
-        String answer = "" + n;
+        // 아래 방법은 간단한데 속도는 느림
+        //  String answer = "" + n;
+        
+        // 개선 방법 1
+        String answer = Integer.toString(n);
         return answer;
     }
 }
