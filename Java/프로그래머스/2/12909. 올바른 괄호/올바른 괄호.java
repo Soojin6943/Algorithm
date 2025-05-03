7f6c9d5c3157fb4a0afcf090d5c586caf0d5078b
Soojin6943 <159032295+Soojin6943@users.noreply.github.com>
2025-05-04 01:17:32 +0900

    [level 2] Title: 올바른 괄호, Time: 18.63 ms, Memory: 54.9 MB -BaekjoonHub

--- Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,31 +1,19 @@
+import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
+        Stack<Character> st = new Stack<>();
 
-        // 1. ( 괄호는 카운트 1 증가
-        // 2. ) 괄호는 카운트 1 다운
-        // 3. 카운트 0 인데 ) 들어오면 실패
-        // 4. 마지막에 카운트 0이면 성공
-        int cnt = 0;
-        
-        for (char test : s.toCharArray()){
-            if (test == '('){
-                cnt ++;
-            } else {
-                if (cnt == 0){
-                    return false;
-                } else {
-                    cnt --;
-                }
-            }
+        for (char ch : s.toCharArray()){
+            if (ch == '('){
+                st.push('(');
+            } else if (ch == ')' && !st.isEmpty()){
+                st.pop();
+            } else return false;
+            
         }
         
-        if (cnt != 0){
-            return false;
-        }
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        //System.out.println("Hello Java");
-
-        return answer;
+        if (st.isEmpty()) return true;
+        else return false;
     }
 }
