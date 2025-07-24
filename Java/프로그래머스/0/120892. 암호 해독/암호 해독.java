2ecc62e74cf1f1dc3e3bbf22962167755f13ec6e
Soojin6943 <159032295+Soojin6943@users.noreply.github.com>
2025-07-25 02:14:41 +0900

    [level 0] Title: 암호 해독, Time: 0.12 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder answer = new StringBuilder();
+        for (int i=code-1; i<cipher.length(); i+=code){
+            char c = cipher.charAt(i);
+            answer.append(c);
+        }
+        return answer.toString();
+    }
+}
