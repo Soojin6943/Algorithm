67d7330f35f8f917626c5cbb0681774d38dd35b1
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2025-09-22 15:16:57 +0900

    [Silver III] Title: 1로 만들기, Time: 196 ms, Memory: 21560 KB -BaekjoonHub

---
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int[] dp = new int[N+1];
+        dp[0] = dp[1] = 0;
+        
+        // 바텀 업
+        for (int i=2; i<=N; i++){
+            // 1뺄때
+            dp[i] = dp[i-1] + 1;
+            
+            // 2로 나눌때
+            if (i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i/2]+1);
+            } 
+            
+            // 3으로 나눌때
+            if (i % 3 == 0){
+                dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            }
+        }
+      
+        
+        System.out.println(dp[N]);
+        
+    }
+}
