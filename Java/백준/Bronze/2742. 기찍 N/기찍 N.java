e3b00d3f091f33b9014b04a7cdeebe9b93d6df38
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2025-09-26 22:44:33 +0900

    [Bronze V] Title: 기찍 N, Time: 264 ms, Memory: 20500 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i=N; i>0; i--){
+            sb.append(i).append("\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
+}
