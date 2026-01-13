270b6b32be8bb92fa7c5f7aa6af9757eb9d19dd9
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2026-01-13 17:54:57 +0900

    [Silver III] Title: N과 M (2), Time: 196 ms, Memory: 18376 KB -BaekjoonHub

---
+++ Java/백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Main {
+    static boolean[] visited;
+    static int[] arr;
+    
+    static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        
+        for (int i=1; i<=N; i++) {
+            if (!visited[i] && (depth == 0 || arr[depth-1] < i)) {
+                visited[i] = true;
+                arr[depth] = i;
+                dfs(N, M, depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        visited = new boolean[N+1];
+        arr = new int[M];
+        
+        dfs(N, M, 0);
+    }
+}
