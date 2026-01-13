63286503c0e22622582325574cc4fce83717ab00
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2026-01-13 17:34:43 +0900

    [Silver IV] Title: 카드2, Time: 236 ms, Memory: 31888 KB -BaekjoonHub

---
+++ Java/백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i=1; i<=N; i++) {
+            dq.add(i);
+        }
+        
+        while (dq.size() > 1) {
+            dq.pollFirst();
+            dq.addLast(dq.pollFirst());
+        }
+        
+        System.out.println(dq.peekFirst());
+    }
+}
