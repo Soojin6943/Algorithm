906dc89ae0805f8661c42e265db6e4d21a966261
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2026-01-24 15:19:47 +0900

    [level 3] Title: 네트워크, Time: 0.84 ms, Memory: 77.8 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,46 @@
+import java.util.*;
+
 class Solution {
-    private static boolean[] visit;
-    private static int[][] computer;
+    // 깊이 우선 탐색 
+    // 연결된 노드 전부 방문 처리 -> 새로운 노드 발견시 네트워크 +1
+    boolean[] visited;
+    List<List<Integer>> graph;
     
-    private static void dfs(int now) {
-        // 현재 노드 방문 처리
-        visit[now] = true;
-        for (int i=0; i<computer[now].length; i++){
-            // 연결되어 있지만 방문하지 않은 노드
-            if (computer[now][i] == 1 && !visit[i]){
+    private void dfs(int n) {
+        visited[n] = true;
+        
+        for (int i : graph.get(n)) {
+            if (!visited[i]) {
                 dfs(i);
             }
         }
-        
     }
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        computer = computers;
-        visit = new boolean[n];
         
-        for (int i=0; i<n; i++){
-            if (!visit[i]){
-                dfs(i);
-                answer ++;
+        visited = new boolean[n];
+        graph = new ArrayList<>();
+        
+        for (int i=0; i<n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (computers[i][j] == 1) {
+                    graph.get(i).add(j);
+                }
             }
         }
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer ++;
+                dfs(i);
+            } 
+        }
+        
         return answer;
     }
 }
