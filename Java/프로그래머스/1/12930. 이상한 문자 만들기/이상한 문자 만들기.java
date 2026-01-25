c96e027376ffe97e6220a28c6c317b6c19bae6e7
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2026-01-26 08:35:36 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.23 ms, Memory: 81 MB -BaekjoonHub

--- Java/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ Java/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,31 +1,20 @@
 class Solution {
     public String solution(String s) {
-        StringBuffer answer = new StringBuffer();
+        String answer = "";
+        String[] str = s.split(" ", -1); // 아직 공백이 연속으로 오는지는 확신 X
         
-        // 공백으로 문자열 배열로 나누기
-        // 문자열 기준으로 대소 변경
-        // 공백이 2개 이상 연속일때 생각하기
+        StringBuilder sb = new StringBuilder();
         
-        // 공백도 받기 위해 -1
-        String[] arr = s.split(" ", -1);
-        
-        for (String str : arr) {    // 나눈 문자별로 수행
-            for (int i=0; i<str.length(); i++){ // 문자열의 문자 바꾸기
-                
-                char change = str.charAt(i);
-                if (i % 2 == 0){ // 짝수 - 대문자
-                    answer.append(Character.toUpperCase(change));
-                } else { // 홀수 - 소문자
-                    answer.append(Character.toLowerCase(change));
-                }
+        for (String word : str) {
+            for (int i=0; i<word.length(); i++) {
+                if (i % 2 == 0) {
+                    sb.append(Character.toUpperCase(word.charAt(i)));
+                } else sb.append(Character.toLowerCase(word.charAt(i)));
             }
-            
-            answer.append(" ");
-            
+            sb.append(" ");
         }
         
-        // 마지막 공백 삭제
-        answer.deleteCharAt(answer.length() -1);
-        return answer.toString();
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
     }
 }
