d167cb3d4aee8bf1a29eef48cacdaedc506ffb8e
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2026-01-26 08:44:20 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,12 +1,17 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for (int i=0; i<commands.length; i++){
-            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(arr);
-            answer[i] = arr[commands[i][2]-1];
+        for (int i=0; i<commands.length; i++) {
+            List<Integer> cut = new ArrayList<>();
+            for (int j=commands[i][0] - 1; j < commands[i][1]; j++) {
+                cut.add(array[j]);
+            }
+            
+            Collections.sort(cut);
+            answer[i] = cut.get(commands[i][2] - 1);
         }
         return answer;
     }
