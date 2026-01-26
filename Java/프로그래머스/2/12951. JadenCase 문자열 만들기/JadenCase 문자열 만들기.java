a5e166d8bcc1e2ef71a34345027f6c4a7c58c140
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2026-01-26 13:13:47 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.20 ms, Memory: 76.3 MB -BaekjoonHub

--- Java/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ Java/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,31 +1,20 @@
+import java.util.*;
+
 class Solution {
-    public String solution(String s) {
-        StringBuffer answer = new StringBuffer();
+    public String solution(String s) {        
+        s = s.toLowerCase();
+        String[] str = s.split(" ", -1);
+        StringBuilder sb = new StringBuilder();
         
-        // -1 해주면 빈 값도 배열에 받을 수 있음
-        String[] arr = s.split(" ", -1);
-
-        for (int i=0; i<arr.length; i++){
-            String a = arr[i];
-            System.out.println(a);
-            
-            // 공백 여러개 이어질 경우 처리하기!!!
-            if (a.length() == 0){
-                answer.append("");
-            } else if(a.charAt(0) < 57 && a.charAt(0) > 48 ){
-                answer.append(a.charAt(0));
-                answer.append(a.substring(1, a.length()).toLowerCase());
-
-            } else {
-                answer.append(Character.toUpperCase(a.charAt(0)));
-                answer.append(a.substring(1, a.length()).toLowerCase());
-
-            }
-            
-            if (i != arr.length -1){
-                answer.append(" ");
-            }
+        for (String word : str) {
+            if (word.length() > 0 && !Character.isDigit(word.charAt(0))) {
+                char Upper = Character.toUpperCase(word.charAt(0));
+                sb.append(Upper).append(word.substring(1));
+            } else sb.append(word);
+            sb.append(" ");
         }
-        return answer.toString();
+        sb.deleteCharAt(sb.length()-1);
+        
+        return sb.toString();
     }
 }
