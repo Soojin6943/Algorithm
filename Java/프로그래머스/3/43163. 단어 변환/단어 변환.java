254fc808087d6d00344011b8d111ac524ea909be
HANSOOJIN <159032295+Soojin6943@users.noreply.github.com>
2026-01-26 16:51:10 +0900

    [level 3] Title: 단어 변환, Time: 0.73 ms, Memory: 83.1 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,58 +1,47 @@
 import java.util.*;
+
 class Solution {
-    class Node {
-        String word;
-        int cnt;
+    boolean visited[];
+    class Word {
+        String name;
+        int depth;
         
-        public Node(String word, int cnt){
-            this.word = word;
-            this.cnt = cnt;
+        Word(String name, int depth) {
+            this.name = name;
+            this.depth = depth;
         }
     }
-    
-    public boolean covert(String begin, String target){
-        int cnt = 0;
+    private int bfs(String begin, String target, String[] words) {
+        Deque<Word> que = new ArrayDeque<>();
+        que.add(new Word(begin, 0));
         
-        for (int i=0; i<begin.length(); i++){
-            if(begin.charAt(i) != target.charAt(i)){
-                cnt ++;
+        while (!que.isEmpty()) {
+            Word cur = que.poll();
+            
+            if (cur.name.equals(target)) {
+                return cur.depth;
+            }
+            
+            for (int j=0; j<words.length; j++) {
+                int cnt = 0;
+                for (int i=0; i<begin.length(); i++) {
+                    if (words[j].charAt(i) != cur.name.charAt(i)) cnt ++;
+                }
+                
+                if (cnt == 1 && !visited[j]) {
+                    visited[j] = true;
+                    que.add(new Word(words[j], cur.depth + 1));
+                }
             }
         }
-        
-        if (cnt == 1) return true;
-        else return false;
+        return 0;
     }
     
     public int solution(String begin, String target, String[] words) {
-        if (!Arrays.asList(words).contains(target)){
-            return 0;
-        }
+        int answer = 0;
         
-        // bfs를 위한 큐 + 방문 여부
-        Queue<Node> que = new LinkedList<>();
-        boolean[] visited = new boolean[words.length];
-        
-        // 시작 노드 추가 (시작 단어, 변환 횟수)
-        que.add(new Node(begin, 0));
-        
-        while(!que.isEmpty()){
-            // 현재 노드 꺼내기
-            Node current = que.poll();
-            
-            // 현재 단어가 타겟이랑 같으면 변환 횟수 반환
-            if (current.word.equals(target)) {
-                return current.cnt;
-            } else {
-                // 단어 배열을 순회하며 다음 단어 찾음
-                for(int i=0; i<words.length; i++){
-                    if(!visited[i] && covert(current.word, words[i])){    // 변환 가능하면  
-                        visited[i] = true;
-                        que.add(new Node(words[i], current.cnt + 1));
-                    }     
-                }
-            }
-        }   
-        // while 문 끝날때까지 못찾으면 0
-        return 0;  
+        visited = new boolean[words.length];
+        answer = bfs(begin, target, words);
+        return answer;
     }
 }
