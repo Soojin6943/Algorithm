b5cb88de980ad6edb15b7de5c838714419553c29
HANSOOJIN <sjhan0711@gmail.com>
2026-05-29 23:54:25 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 6.82 ms, Memory: 65 MB -BaekjoonHub

--- Java/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ Java/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,14 +1,9 @@
 class Solution {
-    public long solution(long n) {
-        long answer = 0;
-        
-        for (long i=0; i*i <= n; i++){
-            if (i*i == n){
-                answer = (i+1) * (i+1);
-                break;
-            }
-            answer = -1;
+    public long solution(long n) {        
+        for (long i=1; i*i <= n ; i++) {
+            if (i * i == n) return (i+1) * (i+1);
+            if (i * i > n) return -1;
         }
-        return answer;
+        return -1;
     }
 }
