8081e999f3194bdc8c8b0b73d9d1305e5199e348
HANSOOJIN <sjhan0711@gmail.com>
2026-06-01 14:59:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.71 ms, Memory: 59.6 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,53 +1,39 @@
 import java.util.*;
 
 class Solution {
-    // 검은색 0 = 이동 불가, 흰색 1 = 이동 가능
-    // 동, 서, 남, 북 이동 가능
-    // 맵 내 이동, 벗어나기 불가능
-    // 상대 진영까지 최소 거리 구하기 <- 너비 우선 탐색
-    // 도착 할 수 없을 때는 -1
-    
-    boolean[][] visited;
-    int[] dn = {0, 0, 1, -1};
-    int[] dm = {1, -1, 0, 0};
-    
     public int solution(int[][] maps) {
-        int answer = 0;
-        int n = maps.length;
-        int m = maps[0].length;
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[]{0, 0, 1});
         
-        visited = new boolean[n][m];
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        visited[0][0] = true;
         
-        answer = bfs(maps, 0, 0, n, m);
-        return answer;
-    }
-    
-    private int bfs(int[][] maps, int sn, int sm, int n, int m) {
-        Deque<int[]> que = new ArrayDeque<>();
-        
-        visited[sn][sm] = true;
-        que.add(new int[]{sn, sm, 1});
+        int[] dx = new int[]{0, 0, 1, -1};
+        int[] dy = new int[]{1, -1, 0, 0};
         
         while(!que.isEmpty()) {
             int[] cur = que.poll();
-            // 상대 진영인지 확인
-            if (cur[0] == n-1 && cur[1] == m-1) {
-                return cur[2];
-            }
+            
             for (int i=0; i<4; i++) {
-
-                // 다음 이동 칸
-                int nn = cur[0] + dn[i];
-                int nm = cur[1] + dm[i];
-
-                if (nn < 0 || nm < 0 || nn >= n || nm >= m) continue;
-
-                if (!visited[nn][nm] && maps[nn][nm] == 1) {
-                    visited[nn][nm] = true;
-                    que.add(new int[]{nn, nm, cur[2] + 1});
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                
+                if (nx < 0 || nx >= maps.length ||
+                   ny < 0 || ny >= maps[0].length
+                   || visited[nx][ny] == true
+                   || maps[nx][ny] == 0) {
+                    continue;
                 }
+                
+                if (nx == maps.length -1 && ny == maps[0].length -1) {
+                    return cur[2] + 1;
+                }
+                
+                que.add(new int[]{nx, ny, cur[2] + 1});
+                visited[nx][ny] = true;
             }
         }
+        
         return -1;
     }
 }
