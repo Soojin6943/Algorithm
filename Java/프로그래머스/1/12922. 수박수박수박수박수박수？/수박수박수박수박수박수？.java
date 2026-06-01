fb2449259e9ad771e88645e4ee695f070dfa787d
HANSOOJIN <sjhan0711@gmail.com>
2026-06-01 11:16:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.43 ms, Memory: 65.7 MB -BaekjoonHub

--- Java/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ Java/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,13 +1,17 @@
+import java.util.*;
+
 class Solution {
     public String solution(int n) {
-        StringBuffer answer = new StringBuffer();
-        for (int i=0; i<n; i++){
-            if (i % 2 == 0){
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i=1; i<=n; i++) {
+            if (i % 2 != 0) {
                 answer.append("수");
             } else {
                 answer.append("박");
             }
         }
+        
         return answer.toString();
     }
 }
