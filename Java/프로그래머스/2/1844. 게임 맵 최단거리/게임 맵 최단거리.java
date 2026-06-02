9af600a1515a4d0172e52ddbe0a66df59bb18e2d
HANSOOJIN <sjhan0711@gmail.com>
2026-06-02 10:32:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.49 ms, Memory: 59.5 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,47 @@
+/**
+필요 
+- 큐 (x, y, depth)
+- 초기 넣기
+- 방문확인
+- 
+*/
 import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{0, 0, 1});
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 1});
         
         boolean[][] visited = new boolean[maps.length][maps[0].length];
         visited[0][0] = true;
         
-        int[] dx = new int[]{0, 0, 1, -1};
-        int[] dy = new int[]{1, -1, 0, 0};
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         
-        while(!que.isEmpty()) {
-            int[] cur = que.poll();
+        while(!queue.isEmpty()) {
+            int[] next = queue.poll();
             
             for (int i=0; i<4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
+                int nx = next[0] + dx[i];
+                int ny = next[1] + dy[i];
                 
-                if (nx < 0 || nx >= maps.length ||
-                   ny < 0 || ny >= maps[0].length
+                if (nx < 0 || nx >= maps.length
+                   || ny < 0 || ny >= maps[0].length
                    || visited[nx][ny] == true
                    || maps[nx][ny] == 0) {
                     continue;
                 }
                 
-                if (nx == maps.length -1 && ny == maps[0].length -1) {
-                    return cur[2] + 1;
-                }
-                
-                que.add(new int[]{nx, ny, cur[2] + 1});
+                queue.add(new int[]{nx, ny, next[2] + 1});
                 visited[nx][ny] = true;
+                
+                if (nx == maps.length - 1 && ny == maps[0].length -1) {
+                    return next[2] + 1;
+                }
             }
         }
         
         return -1;
     }
+    
 }
