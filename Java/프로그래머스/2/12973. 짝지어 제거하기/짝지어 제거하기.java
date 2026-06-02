cddf141a104f6ef677e71a1171b067a77cc672df
HANSOOJIN <sjhan0711@gmail.com>
2026-06-02 17:43:41 +0900

    [level 2] Title: 짝지어 제거하기, Time: 49.82 ms, Memory: 69.4 MB -BaekjoonHub

--- Java/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ Java/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,21 +1,36 @@
+/**
+- 소문자 문자열
+- 같은 알파벳이 2개 붙어 있는 짝
+- 그 둘을 제거 후 앞뒤 이어 붙이기
+- 성공 1, 실패 0
+*/
+
 import java.util.*;
 
 class Solution
 {
     public int solution(String s)
     {
+        // 스택 사용
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()){
-            if (stack.isEmpty()){
+        
+        for (char c : s.toCharArray()) {
+            
+            if(stack.isEmpty()) {
                 stack.push(c);
-            } else if (stack.peek() == c) {
-                stack.pop();
             } else {
-                stack.push(c);
+                if (c == stack.peek()) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
             }
         }
-        
-        if (!stack.isEmpty()) return 0;
-        return 1;
+    
+        if (stack.isEmpty()){
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
