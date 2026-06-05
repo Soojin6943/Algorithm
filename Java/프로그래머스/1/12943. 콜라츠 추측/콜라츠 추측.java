0c59743a36fcfcd723cbab065c293413497c30d6
HANSOOJIN <sjhan0711@gmail.com>
2026-06-05 23:04:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 66.6 MB -BaekjoonHub

--- Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,23 +1,21 @@
 class Solution {
     public int solution(int num) {
-        // int로 하면 정수 오버플로우 발생으로 인해 실패
-        long answer = num;
         int cnt = 0;
         
-        if (answer == 1) return 0;
+        if (num == 1) return 0;
+        long n = num;
         
-        while (answer != 1){
+        while (n != 1) {
             if (cnt >= 500) return -1;
             
-            if (answer % 2 == 0){
-                answer = answer / 2;
-                cnt ++;
+            if (n % 2 == 0) {
+                n /= 2;
             } else {
-                answer = answer * 3 + 1;
-                cnt ++;
+                n = n * 3 + 1;
             }
-            
+            cnt++;
         }
+        
         return cnt;
     }
 }
