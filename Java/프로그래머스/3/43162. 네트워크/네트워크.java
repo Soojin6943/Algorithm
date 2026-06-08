5865ba7b16e82d8105669187e25425aabfd6f8c0
HANSOOJIN <sjhan0711@gmail.com>
2026-06-08 15:03:09 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 88.4 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,27 @@
-import java.util.*;
-
 class Solution {
     
-    public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];        
-        Queue<Integer> que = new LinkedList<>();
+    public static void dfs(int now, int n, int[][] computers, boolean[] visited) {
+        visited[now] = true;
         
-        // 네트워크 갯수
-        int cnt = 0;
-        
-        // 모든 컴퓨터에서 시작
-        for (int i=0; i<n; i++) { 
-            if (visited[i]) continue;
-            
-            if (!visited[i]) {
-                visited[i] = true;
-                cnt ++;
+        for (int i=0; i<n; i++) {
+            if (computers[now][i] == 1 && !visited[i]) {
+                dfs(i, n, computers, visited);
             }
-            
-            que.add(i);
-            
-            while (!que.isEmpty()) {
-                int now = que.poll();
-                
-                for (int j=0; j<n; j++) {
-                    if (computers[now][j] == 1 && !visited[j]) {
-                        que.add(j);
-                        visited[j] = true;
-                    }
-                }
+        }
+    }
+    
+    public int solution(int n, int[][] computers) {
+    
+        boolean[] visited = new boolean[n];
+        int network = 0;
+        
+        for (int i=0; i<n; i++) {
+            if (!visited[i]) {
+                dfs(i, n, computers, visited);
+                network ++;
             }
         }
         
-        return cnt;
+        return network;
     }
 }
