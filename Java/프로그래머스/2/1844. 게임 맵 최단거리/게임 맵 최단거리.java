3728c7e57c435bab03dc6745866a54e1d25b7141
HANSOOJIN <sjhan0711@gmail.com>
2026-06-08 21:53:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.19 ms, Memory: 58.1 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,30 +1,38 @@
 /**
-필요 
-- 큐 (x, y, depth)
-- 초기 넣기
-- 방문확인
-- 
+너비 우선 탐색 
+
+도착할 수 없을 때 -1
+지나가는 칸 최솟값 구하기
+1은 벽이 없는 자리
 */
+
 import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1});
-        
+        // 기본 세팅
+        Queue<int[]> que = new LinkedList<>();
         boolean[][] visited = new boolean[maps.length][maps[0].length];
+        
+        // 시작점 세팅
+        que.add(new int[]{0, 0, 1});
         visited[0][0] = true;
         
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
+        // 갈 수 있는 방향 좌표
+        int[] dx = new int[]{1, -1, 0, 0};
+        int[] dy = new int[]{0, 0, 1, -1};
         
-        while(!queue.isEmpty()) {
-            int[] next = queue.poll();
+        // 너비 우선 탐색
+        while(!que.isEmpty()) {
+            int[] cur = que.poll();
             
             for (int i=0; i<4; i++) {
-                int nx = next[0] + dx[i];
-                int ny = next[1] + dy[i];
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                // 목적지
+                if (nx == maps.length -1 && ny == maps[0].length -1) return cur[2] + 1;
                 
+                // 불가능
                 if (nx < 0 || nx >= maps.length
                    || ny < 0 || ny >= maps[0].length
                    || visited[nx][ny] == true
@@ -32,16 +40,10 @@ class Solution {
                     continue;
                 }
                 
-                queue.add(new int[]{nx, ny, next[2] + 1});
+                que.add(new int[]{nx, ny, cur[2] + 1});
                 visited[nx][ny] = true;
-                
-                if (nx == maps.length - 1 && ny == maps[0].length -1) {
-                    return next[2] + 1;
-                }
             }
         }
-        
         return -1;
     }
-    
 }
