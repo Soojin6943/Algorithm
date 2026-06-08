482334e915289ed0c8364c19837baa6226e09801
HANSOOJIN <sjhan0711@gmail.com>
2026-06-08 21:40:20 +0900

    [level 2] Title: 타겟 넘버, Time: 3.26 ms, Memory: 79 MB -BaekjoonHub

--- Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,29 @@
+/**
+깊이 우선 탐색 - 모든 넘버 다 써야함
++ / - 경우 탐색
+
+최종 값이 target 이면 카운트
+*/
 class Solution {
-    int answer = 0;
+    static int cnt = 0;
     
-    public void dfs (int sum, int i, int target, int lastIdx, int[] numbers) {
-        if (i == lastIdx) {
-            if (sum == target) answer++;
+    public static void dfs(int now, int depth, int target, int[] numbers) {
+        
+        // 종료 조건
+        if(depth == numbers.length) {
+            if (now == target) cnt ++;
             return;
         }
         
-        dfs(sum + numbers[i], i+1, target, lastIdx, numbers);
-        dfs(sum - numbers[i], i+1, target, lastIdx, numbers);
+        dfs(now + numbers[depth], depth + 1, target, numbers);
+        dfs(now - numbers[depth], depth + 1, target, numbers);
     }
+    
     public int solution(int[] numbers, int target) {
         
-        dfs(0, 0, target, numbers.length, numbers);
+        dfs(0, 0, target, numbers);
         
-        return answer;
+        return cnt;
+        
     }
 }
