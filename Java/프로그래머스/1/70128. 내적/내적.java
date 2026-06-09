326f4597eff56bfb12ba6235cd62fd79ffe5e1bf
HANSOOJIN <sjhan0711@gmail.com>
2026-06-09 15:59:42 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

--- Java/프로그래머스/1/70128. 내적/내적.java
+++ Java/프로그래머스/1/70128. 내적/내적.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
+        int result = 0;
         
-        for (int i=0; i<a.length; i++){
-            answer += a[i] * b[i];
+        for (int i=0; i<a.length; i++) {
+            result += a[i] * b[i];
         }
-        return answer;
+        
+        return result;
     }
 }
