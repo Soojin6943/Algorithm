f356e82b64afe2f0886ece2f57f9b5aacb624aec
HANSOOJIN <sjhan0711@gmail.com>
2026-06-09 15:57:40 +0900

    [level 2] Title: 타겟 넘버, Time: 17.72 ms, Memory: 88.5 MB -BaekjoonHub

--- Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,27 @@
 /**
-깊이 우선 탐색 - 모든 넘버 다 써야함
-+ / - 경우 탐색
-
-최종 값이 target 이면 카운트
-*/
+완전 탐색 = 깊이 우선 탐색
+- 스택 or 재귀
+- 더하거나 빼거나
+*/ 
 class Solution {
     static int cnt = 0;
-    
-    public static void dfs(int now, int depth, int target, int[] numbers) {
-        
-        // 종료 조건
-        if(depth == numbers.length) {
-            if (now == target) cnt ++;
+    public static void dfs(int[] numbers, int target, int depth, int now) {
+        // 종료 조건 (마지막 결과가 타겟과 같으면 카운트)
+        if (numbers.length == depth) {
+            if (now == target) {
+                cnt++;
+                System.out.println(cnt);
+            }
             return;
         }
         
-        dfs(now + numbers[depth], depth + 1, target, numbers);
-        dfs(now - numbers[depth], depth + 1, target, numbers);
+        dfs(numbers, target, depth + 1, now + numbers[depth]);
+        dfs(numbers, target, depth + 1, now - numbers[depth]);
     }
     
     public int solution(int[] numbers, int target) {
-        
-        dfs(0, 0, target, numbers);
+        dfs(numbers, target, 0, 0);
         
         return cnt;
-        
     }
 }
