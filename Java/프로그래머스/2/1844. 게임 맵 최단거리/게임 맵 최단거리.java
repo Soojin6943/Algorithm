541185e6b56117b99a040c92bf348c1c595f33cc
HANSOOJIN <sjhan0711@gmail.com>
2026-06-09 15:45:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.47 ms, Memory: 57.6 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,45 @@
 /**
-너비 우선 탐색 
+너비 우선 탐색
 
-도착할 수 없을 때 -1
-지나가는 칸 최솟값 구하기
-1은 벽이 없는 자리
+1 = 벽 없는 자리
+도착 불가 = -1 출력
 */
 
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] maps) {
-        // 기본 세팅
-        Queue<int[]> que = new LinkedList<>();
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        
-        // 시작점 세팅
-        que.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
-        
-        // 갈 수 있는 방향 좌표
-        int[] dx = new int[]{1, -1, 0, 0};
-        int[] dy = new int[]{0, 0, 1, -1};
-        
-        // 너비 우선 탐색
+    static int[] dx = new int[]{0, 0, 1, -1};
+    static int[] dy = new int[]{1, -1, 0, 0};
+    
+    public static int bfs(Deque<int[]> que, boolean[][] visited, int[][] maps) {
         while(!que.isEmpty()) {
-            int[] cur = que.poll();
+            int[] cur = que.pollFirst();
             
             for (int i=0; i<4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
-                // 목적지
-                if (nx == maps.length -1 && ny == maps[0].length -1) return cur[2] + 1;
                 
-                // 불가능
-                if (nx < 0 || nx >= maps.length
+                if (nx == maps.length - 1 && ny == maps[0].length - 1) return cur[2] + 1;
+                
+                if (nx < 0 || nx >= maps.length 
                    || ny < 0 || ny >= maps[0].length
                    || visited[nx][ny] == true
-                   || maps[nx][ny] == 0) {
-                    continue;
-                }
+                   || maps[nx][ny] == 0) continue;
                 
-                que.add(new int[]{nx, ny, cur[2] + 1});
+                que.offer(new int[]{nx, ny, cur[2] + 1});
                 visited[nx][ny] = true;
             }
         }
         return -1;
+    }
+    
+    public int solution(int[][] maps) {
+        Deque<int[]> que = new ArrayDeque<>();
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        
+        que.offer(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        
+        return bfs(que, visited, maps);
     }
 }
