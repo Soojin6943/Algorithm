89f0d057536d82606b8d5cdbab3127f835f74531
HANSOOJIN <sjhan0711@gmail.com>
2026-06-10 14:43:02 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 82.6 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,13 +1,6 @@
-/**
-깊이 우선 탐색
+// 깊이 우선 탐색 - 스택, 재귀
 
-- 연결된 노드 확인
-- 방문 확인
-
-스택, 재귀
-*/
 class Solution {
-    
     
     public static void dfs(int now, boolean[] visited, int[][] computers) {
         visited[now] = true;
@@ -17,20 +10,21 @@ class Solution {
                 dfs(i, visited, computers);
             }
         }
-            
     }
     
     public int solution(int n, int[][] computers) {
+        
         boolean[] visited = new boolean[n];
         int cnt = 0;
         
         for (int i=0; i<n; i++) {
-            if (visited[i] == false) {
+            if (!visited[i]) {
+                dfs(i, visited, computers);
                 cnt++;
-                dfs(i, visited, computers); 
             }
         }
         
         return cnt;
+        
     }
 }
