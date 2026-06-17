020bda3c156eb3c7ee6df29d9d44278dff523622
HANSOOJIN <sjhan0711@gmail.com>
2026-06-17 10:46:56 +0900

    [level 2] Title: 타겟 넘버, Time: 2.93 ms, Memory: 74.9 MB -BaekjoonHub

--- Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,37 +1,27 @@
-/**
-원래라면 깊이 우선 탐색
-하지만 너비 우선 탐색으로 연습
-- 큐
-- 방문 확인
+/** 
+깊이 우선 탐색
+- 스택, 재귀
+
 */
-
-import java.util.*;
-
 class Solution {
-    public int solution(int[] numbers, int target) {
-        // 현재 결과, 인덱스
-        Deque<int[]> que = new ArrayDeque<>(); 
-        
-        // 초기 세팅
-        que.offer(new int[]{0, 0});
-        int cnt = 0;
-        
-        while (!que.isEmpty()) {
-            int[] cur = que.poll();
-            int now = cur[0];
-            int idx = cur[1];
-            
-            // 종료 조건
-            if (numbers.length == idx) {
-                if (now == target) {
-                    cnt ++;
-                }
-                continue;
+    
+    static int cnt = 0;
+    
+    static void dfs(int now, int idx, int[] numbers, int target) {
+        if (idx == numbers.length) {
+            if (now == target) {
+                cnt ++;
             }
-            
-            que.add(new int[]{now + numbers[idx], idx + 1});
-            que.add(new int[]{now - numbers[idx], idx + 1});
+            return;
         }
+        dfs(now + numbers[idx], idx + 1, numbers, target);
+        dfs(now - numbers[idx], idx + 1, numbers, target);
+        
+    }
+    
+    public int solution(int[] numbers, int target) {
+        
+        dfs(0, 0, numbers, target);
         
         return cnt;
     }
